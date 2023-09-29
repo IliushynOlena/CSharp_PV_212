8a7a430efe2e6fe819a849a7d6dd815f5b973321
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-09-29 20:07:00 +0300

    String

---
+++ 05_StringBuilder/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+
+namespace _05_StringBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            string str = "Hello";//5b
+            Console.WriteLine(str.Length);
+
+
+            StringBuilder builder = new StringBuilder();
+            Console.WriteLine($" Length : {builder.Length}"  );
+            Console.WriteLine($" Capacity : {builder.Capacity}"  );
+
+            builder.Append("bla");
+            Console.WriteLine($" Length : {builder.Length}");
+            Console.WriteLine($" Capacity : {builder.Capacity}");
+            builder.Append("bla");
+            Console.WriteLine($" Length : {builder.Length}");
+            Console.WriteLine($" Capacity : {builder.Capacity}");
+            builder.Append("bla");
+            Console.WriteLine($" Length : {builder.Length}");
+            Console.WriteLine($" Capacity : {builder.Capacity}");
+            builder.Append("bla");
+            Console.WriteLine($" Length : {builder.Length}");
+            Console.WriteLine($" Capacity : {builder.Capacity}");
+            builder.Append("bla");
+            Console.WriteLine($" Length : {builder.Length}");
+            Console.WriteLine($" Capacity : {builder.Capacity}");
+            builder.Append("bla");
+            Console.WriteLine($" Length : {builder.Length}");
+            Console.WriteLine($" Capacity : {builder.Capacity}");
+            builder.Append("bla");
+            Console.WriteLine($" Length : {builder.Length}");
+            Console.WriteLine($" Capacity : {builder.Capacity}");
+            builder.AppendLine("bla");
+            Console.WriteLine($" Length : {builder.Length}");
+            Console.WriteLine($" Capacity : {builder.Capacity}");
+            builder.Append("bla");
+            builder.Append("bla");
+            builder.Append("bla");
+            builder.Append("bla");
+            Console.WriteLine($" Length : {builder.Length}");
+            Console.WriteLine($" Capacity : {builder.Capacity}");
+            builder.Append("bla");
+            builder.Append("bla");
+            builder.Append("bla");
+            Console.WriteLine($" Length : {builder.Length}");
+            Console.WriteLine($" Capacity : {builder.Capacity}");
+            Console.WriteLine(builder);
+
+
+
+        }
+    }
+}
